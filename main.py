d255cd228cd65f2d3155b2fda36b0d19c274a2d3
NVB20 <nivb49@gmail.com>
2025-08-26 12:28:23 +0300

    run all toghter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium_read import open_whatsapp
-
+from render_message import message_formatter
+from sheets_update import update_sheets
 
 if __name__ == "__main__":
     msgs = open_whatsapp()
+    msgs = message_formatter(msgs)
+    update_sheets(msgs)
