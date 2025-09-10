fd898c57babd15f8659bbe78b6d6ee104773f98d
NVB20 <nivb49@gmail.com>
2025-09-10 11:06:34 +0300

    write last time updated

---
+++ sheets_last_update.py
@@ -0,0 +1,30 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+def last_time_updated():
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+
+    creds = Credentials.from_service_account_file("sheets-api-cred.json", scopes=scopes)
+    client = gspread.authorize(creds)
+
+    # Load .env file
+    load_dotenv()
+    sheet_id = os.getenv("SHEET_ID")
+
+    if not sheet_id:
+        raise ValueError("SHEET_ID not found in environment variables!")
+
+    print(f"Loaded Sheet ID: {sheet_id}")
+
+    sheet = client.open_by_key(sheet_id)
+    worksheet = sheet.worksheet("dashboard")
+
+    current_datetime = datetime.now().strftime("%d-%m %H:%M")
+    worksheet.update_acell('C9', f"'{current_datetime}")
+    print(f"Updated cell C9 with: {current_datetime}")
+
+if __name__ == "__main__":
+    last_time_updated()
