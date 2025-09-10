5f8c5d08deb4be2f15c28cad97ced896bfcb1224
NVB20 <nivb49@gmail.com>
2025-09-10 11:07:47 +0300

    last time update

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from selenium_read import open_whatsapp
 from render_message import message_formatter
 from sheets_update import update_sheets
+from sheets_last_update import last_time_updated
 
 if __name__ == "__main__":
     msgs = open_whatsapp()
     msgs = message_formatter(msgs)
     update_sheets(msgs)
+    last_time_updated()
