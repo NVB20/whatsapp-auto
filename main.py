917612f58270381412abeb3635264702843e19ed
NVB20 <nivb49@gmail.com>
2025-10-29 18:53:57 +0200

    failed message read log

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from render_message import message_formatter
 from sheets_update import update_sheets
 from sheets_last_update import last_time_updated
 from download_csv_backup import download_data_to_folder
-from time_log import timed, table_log, setup_handler
+from time_log import timed, table_log, setup_handler, no_messages
 
 
 # === Configuration ===
@@ -29,6 +29,10 @@ if __name__ == "__main__":
     elapsed, msgs = timed("open_whatsapp", open_whatsapp)
     runtimes["open_whatsapp"] = elapsed
 
+if len(msgs) == 0:
+    no_messages(elapsed)
+    
+else:
     elapsed, msgs = timed("message_formatter", message_formatter, msgs)
     runtimes["message_formatter"] = elapsed
 
